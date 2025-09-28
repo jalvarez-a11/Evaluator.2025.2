ddfb3dcc8ca88452bd897837d98dabf1e6b6b9ec
Jose Manuel <jalvarezalvarez30@gmail.com>
2025-09-27 19:07:11 -0500

    usar mas de un digito y decimales

--- Evaluator/Evaluator.Core/ExpressionEvaluator.cs
+++ Evaluator/Evaluator.Core/ExpressionEvaluator.cs
@@ -5,103 +5,140 @@ public class ExpressionEvaluator
     public static double Evaluate(string infix)
     {
         var postfix = InfixToPostfix(infix);
-        return Calulate(postfix);
+        Console.WriteLine($"Postfix: {postfix}");
+        return Calculate(postfix);
     }
 
     private static string InfixToPostfix(string infix)
     {
         var stack = new Stack<char>();
-        var postfix = string.Empty;
-        foreach (char item in infix)
+        var postfixBuilder = new System.Text.StringBuilder();
+
+        for (int i = 0; i < infix.Length; i++)
         {
-            if (IsOperator(item))
+            char item = infix[i];
+
+            // with decimals
+            if (char.IsDigit(item) || item == '.')
             {
-                if (item == ')')
+                while (i < infix.Length && (char.IsDigit(infix[i]) || infix[i] == '.'))
                 {
-                    do
+                    postfixBuilder.Append(infix[i]);
+                    i++;
+                }
+                postfixBuilder.Append(' ');
+                i--; // to check the space between chars
+            }
+            else if (item == '-' && (i == 0 || IsOperator(infix[i - 1]) && infix[i - 1] != ')'))
+            {
+                postfixBuilder.Append("0 "); 
+                stack.Push('-');
+            }
+            else if (IsOperator(item))
+            {
+                if (item == '(')
+                {
+                    stack.Push(item);
+                }
+                else if (item == ')')
+                {
+                    while (stack.Count > 0 && stack.Peek() != '(')
                     {
-                        postfix += stack.Pop();
-                    } while (stack.Peek() != '(');
-                    stack.Pop();
+                        postfixBuilder.Append(stack.Pop()).Append(' ');
+                    }
+                    if (stack.Count > 0 && stack.Peek() == '(')
+                    {
+                        stack.Pop();
+                    }
                 }
                 else
                 {
-                    if (stack.Count > 0)
+                    while (stack.Count > 0 && PriorityInfix(item) <= PriorityStack(stack.Peek()))
                     {
-                        if (PriorityInfix(item) > PriorityStack(stack.Peek()))
-                        {
-                            stack.Push(item);
-                        }
-                        else
-                        {
-                            postfix += stack.Pop();
-                            stack.Push(item);
-                        }
+                        postfixBuilder.Append(stack.Pop()).Append(' ');
                     }
-                    else
-                    {
-                        stack.Push(item);
-                    }
+                    stack.Push(item);
                 }
             }
-            else
+            else if (item == ' ')
             {
-                postfix += item;
+                continue;
             }
         }
+
+        // to put the stack empty
         while (stack.Count > 0)
         {
-            postfix += stack.Pop();
+            postfixBuilder.Append(stack.Pop()).Append(' ');
         }
-        return postfix;
+
+        return postfixBuilder.ToString().Trim();
     }
 
-    private static bool IsOperator(char item) => item is '^' or '/' or '*' or '%' or '+' or '-' or '(' or ')';
-
-    private static int PriorityInfix(char op) => op switch
-    {
-        '^' => 4,
-        '*' or '/' or '%' => 2,
-        '-' or '+' => 1,
-        '(' => 5,
-        _ => throw new Exception("Invalid expression."),
-    };
-
-    private static int PriorityStack(char op) => op switch
-    {
-        '^' => 3,
-        '*' or '/' or '%' => 2,
-        '-' or '+' => 1,
-        '(' => 0,
-        _ => throw new Exception("Invalid expression."),
-    };
-
-    private static double Calulate(string postfix)
+    private static double Calculate(string postfix)
     {
         var stack = new Stack<double>();
-        foreach (char item in postfix)
+        var tokens = postfix.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var token in tokens)
         {
-            if (IsOperator(item))
+            if (token.Length == 1 && IsOperator(token[0]) && token[0] != '(' && token[0] != ')')
             {
+                if (stack.Count < 2)
+                    throw new Exception("Expresión inválida: operandos insuficientes.");
+
                 var op2 = stack.Pop();
                 var op1 = stack.Pop();
-                stack.Push(Calulate(op1, item, op2));
+                stack.Push(Calculate(op1, token[0], op2));
             }
             else
             {
-                stack.Push(Convert.ToDouble(item.ToString()));
+                if (double.TryParse(token, System.Globalization.NumberStyles.Any,
+                    System.Globalization.CultureInfo.InvariantCulture, out double number))
+                {
+                    stack.Push(number);
+                }
+                else
+                {
+                    throw new Exception($"Operando inválido: {token}");
+                }
             }
         }
+
+        if (stack.Count != 1)
+            throw new Exception("Expresión inválida.");
+
         return stack.Peek();
     }
 
-    private static double Calulate(double op1, char item, double op2) => item switch
+    private static double Calculate(double op1, char op, double op2) => op switch
     {
         '*' => op1 * op2,
-        '/' => op1 / op2,
+        '/' => op2 == 0 ? throw new DivideByZeroException() : op1 / op2,
         '^' => Math.Pow(op1, op2),
         '+' => op1 + op2,
         '-' => op1 - op2,
-        _ => throw new Exception("Invalid expression."),
+        _ => throw new Exception("Operador inválido."),
+    };
+
+    // Helpers
+    private static bool IsOperator(char c) =>
+        c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '(' || c == ')';
+
+    private static int PriorityInfix(char c) => c switch
+    {
+        '+' or '-' => 1,
+        '*' or '/' => 2,
+        '^' => 3,
+        _ => 0
+    };
+
+    private static int PriorityStack(char c) => c switch
+    {
+        '+' or '-' => 1,
+        '*' or '/' => 2,
+        '^' => 4,   // the most important priority
+        '(' => 0,
+        _ => 0
     };
 }
